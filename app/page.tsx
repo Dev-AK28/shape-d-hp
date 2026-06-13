62ad8909f2c2ff32523927142e6ff6cc5affc5d5
Dev-AK28 <kota.icehockey2016@gmail.com>
2026-06-13 18:41:32 +0900

    feat: add Octaboot-style scroll animations (#16)

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,19 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import MissionVision from '@/components/MissionVision';
 import Navigation from '@/components/Navigation';
+import ScrollReveal from '@/components/scroll/ScrollReveal';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-[#0A192F] to-black">
       <Navigation />
       <Hero />
-      <About />
-      <MissionVision />
+      <ScrollReveal>
+        <About />
+      </ScrollReveal>
+      <ScrollReveal delay={0.1}>
+        <MissionVision />
+      </ScrollReveal>
     </main>
   );
 }
